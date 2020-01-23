4e9130f2b051326540ac41ba7766d617f00e7fa5
David_Izosimov <David.Izosimov@gmail.com>
2020-01-23 16:28:05 +0100

    Adds stripe dependency and creates Landing page component.

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+
+const HomePage = () => {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HomePage
